077817e1d6344c8ff30b99fbc22bda5090d78f58
Haska Steltenpohl <haskasteltenpohl@gmail.com>
2012-10-05 23:43:21 +0200

    updated StarAssualt.java

---
+++ star-assault/src/net/obviam/starassault/StarAssault.java
@@ -0,0 +1,13 @@
+package net.obviam.starassault;
+
+import net.obviam.starassault.screens.GameScreen;
+
+import com.badlogic.gdx.Game;
+
+public class StarAssault extends Game {
+
+	public void create() {
+		setScreen(new GameScreen());
+	} 
+
+}
